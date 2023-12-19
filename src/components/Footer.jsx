9a72d7961ce7f413046b1cf79ad8f36ce1ad19c2
Ankit Agarwal <madhusudanjain@Madhusudans-MacBook-Air.local>
2023-12-19 16:30:11 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        <div>
+            <h1>Techystar</h1>
+            <p>@all rights reserved</p>
+        </div>
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <a href="https://www.linkedin.com/in/ankit-agarwal-9969711a0/" target={"blank"}>LinkedIn</a>
+                <a href="https://www.instagram.com/AnkitAgrawal623" target={"blank"}>Instagram</a>
+                <a href="https://www.github.com/Ankitagrawal623" target={"blank"}>GitHub</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
